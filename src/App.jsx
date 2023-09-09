3e75963aa7e047ac1419ad9bc82d82f6c17040e5
Mihir Bhatkar <mihirbhatkar87@gmail.com>
2023-09-09 22:31:35 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import data from "./data.json";
 
 function App() {
@@ -8,15 +7,16 @@ function App() {
     total += item.amount;
     maxAmount = maxAmount < item.amount ? item.amount : maxAmount;
   }
+  const scalingFactor = total / maxAmount; // done so that the bar graph looks bigger
 
   return (
     <>
       <div className="grid min-h-screen place-items-center bg-neu-cream">
-        <article className="flex max-w-md flex-col gap-4 ">
+        <article className="flex max-w-md flex-col gap-4 p-4">
           <div className="bg-prim-soft-red p-6  rounded-xl shadow-md items-center justify-between flex  w-full">
             <div className="flex flex-col gap-2 text-white h-full ">
               <span className="font-[400] text-sm">My balance</span>
-              <span className="text-3xl font-[700] ">$921.48</span>
+              <span className="text-3xl font-[700]">$921.48</span>
             </div>
             <img className="" src="./logo.svg" alt="company-logo" />
           </div>
@@ -25,20 +25,25 @@ function App() {
               Spending - Last 7 days
             </h1>
 
-            {/*GRAPHS*/}
-            <div className="grid grid-cols-7 w-full h-56 gap-3 items-end">
+            <div
+              style={{ height: 200 }}
+              className="grid mt-4 grid-cols-7 gap-3 w-full items-end"
+            >
               {data.map((item) => {
-                // h-56 is 224px
                 return (
-                  <div className="flex flex-col gap-1 text-sm items-center text-gray-400">
+                  <div
+                    key={item.amount}
+                    className={`flex flex-col gap-1 text-sm items-center relative before:hover:content-[attr(data-tip)] before:p-2 before:absolute before:top-[-44px] before:content-none before:text-white before:bg-black before:rounded text-gray-400`}
+                    data-tip={`$${item.amount}`}
+                  >
                     <canvas
-                      height={(item.amount / total) * 224 * 3}
-                      width="50"
+                      height={(item.amount / total) * 200 * scalingFactor}
+                      width="35"
                       className={`${
                         item.amount === maxAmount
                           ? "bg-prim-cyan"
                           : "bg-prim-soft-red"
-                      } rounded w-full `}
+                      } rounded hover:cursor-pointer hover:opacity-70 `}
                     ></canvas>
                     <span>{item.day}</span>
                   </div>
@@ -46,7 +51,7 @@ function App() {
               })}
             </div>
 
-            <hr className="border mt-2 border-gray-200 rounded-xl w-full" />
+            <hr className="border mt-8 border-gray-200 rounded-xl w-full" />
             <div className="flex flex-col my-2 w-full ">
               <span className="self-start text-sm text-gray-400">
                 Total this month
